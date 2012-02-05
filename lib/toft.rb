0d6a96cc7051fba0e1f91a99de1d600122d1cca5
Francisco Trindade <frank.trindade@gmail.com>
2012-02-06 09:48:27 +1100

    probably not working, but need to save state now

--- lib/toft.rb
+++ lib/toft.rb
@@ -4,6 +4,7 @@ require 'toft/chef/chef_attributes'
 require 'toft/chef/chef_runner'
 require 'toft/puppet/puppet_runner'
 require 'toft/lxc_command'
+require 'toft/command_executor'
 
 module Toft
   DYNAMIC_IP = "0.0.0.0"
@@ -14,7 +15,8 @@ module Toft
   end
   
   def create_node(hostname, options = {})
-    NodeController.instance.create_node(hostname, options)
+    lxc_command = Toft::LxcCommand.new(Toft::CommandExecutor.new)
+    NodeController.instance.create_node(lxc_command, hostname, options)
   end
   
   def find(name)
